6c1d21637c69e84dc6def4c4827d724ae216616c
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-13 16:06:51 +0200

    expand rust config

--- init.el
+++ init.el
@@ -53,8 +53,14 @@
 
 ;; rust development
 (use-package rust-mode)
-(use-package racer)
+(use-package racer
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (setq racer-rust-src-path "~/src/rust/src")
+  :diminish racer-mode)
+
 (use-package cargo
   :config
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
-  (setq compilation-ask-about-save nil))
+  (setq compilation-ask-about-save nil)
+  :diminish cargo-minor-mode)
