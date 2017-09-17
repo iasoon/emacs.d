deed40da6146d2d6c2437c241513b7a2f5638665
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-17 18:28:40 +0200

    extract rust config

--- init.el
+++ init.el
@@ -11,8 +11,12 @@
 (setq use-package-always-ensure t)
 
 (setq config-dir (file-name-directory load-file-name))
+
 ;; add core modules to load path
 (add-to-list 'load-path (expand-file-name "core" config-dir))
+
+;; add language modules to load path
+(add-to-list 'load-path (expand-file-name "lang" config-dir))
 
 ;; Don't save backups to current directory
 (setq backup-directory-alist
@@ -22,20 +26,10 @@
   :bind
   ("C-x g" . magit-status))
 
-;; rust development
-(use-package rust-mode)
-(use-package racer
-  :config
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (setq racer-rust-src-path "~/src/rust/src")
-  :diminish racer-mode)
-
-(use-package cargo
-  :config
-  (add-hook 'rust-mode-hook 'cargo-minor-mode)
-  (setq compilation-ask-about-save nil)
-  :diminish cargo-minor-mode)
-
+;; core
 (require 'iasoon-general)
 (require 'iasoon-appearance)
 (require 'iasoon-ivy)
+
+;; languages
+(require 'iasoon-rust)
