fae1568d8657218850668a5c77725d3d4f466537
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-23 17:50:17 +0200

    use smartparens

--- core/iasoon-editor.el
+++ core/iasoon-editor.el
@@ -1,4 +1,3 @@
-
 ;; avoid buffers with the same name
 (use-package uniquify
   ;; uniquify package is built-in
@@ -11,5 +10,17 @@
   ;; leave special buffers alone
   (setq uniquify-ignore-buffers-re "^\\*")
   )
+
+(use-package smartparens
+  :config
+  (require 'smartparens-config)
+  (setq sp-base-key-bindings 'paredit)
+  (setq sp-autoskip-closing-pair 'always)
+  (setq sp-hybrid-kill-entire-symbol nil)
+  (sp-use-paredit-bindings)
+
+  (show-smartparens-global-mode +1)
+  (smartparens-global-mode +1)
+  :diminish smartparens-mode)
 
 (provide 'iasoon-editor)
