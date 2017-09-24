17bf0a28118ae35837bff0dcd3c91cf114829ef3
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-24 23:26:01 +0200

    js2-mode global externs

--- lang/iasoon-javascript.el
+++ lang/iasoon-javascript.el
@@ -4,7 +4,9 @@
 (use-package json-mode)
 
 (use-package js2-mode
-  :mode (("\\.js$" . js2-mode)))
+  :mode (("\\.js$" . js2-mode))
+  :config
+  (setq js2-global-externs '("module" "require")))
 
 ;; for javascript runtime integration
 (use-package indium)
