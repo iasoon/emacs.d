3aa97aed71dce40409d1cf82036de93948217818
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-24 23:42:29 +0200

    add tern-mode

--- lang/iasoon-javascript.el
+++ lang/iasoon-javascript.el
@@ -8,6 +8,16 @@
   :config
   (setq js2-global-externs '("module" "require")))
 
+(use-package tern
+  :after js2-mode
+  :config
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t))))
+
+(use-package company-tern
+  :after tern
+  :config
+  (add-to-list 'company-backends 'company-tern))
+
 ;; for javascript runtime integration
 (use-package indium)
 
