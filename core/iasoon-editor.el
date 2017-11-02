0ea95ce008973caccab7810926fe112c9f6eb4bc
Ilion Beyst <ilion.beyst@gmail.com>
2017-11-02 12:46:37 +0100

    multiple-cursors

--- core/iasoon-editor.el
+++ core/iasoon-editor.el
@@ -28,4 +28,33 @@
 (use-package expand-region
   :bind ("C-r" . er/expand-region))
 
+(use-package hydra)
+
+(use-package multiple-cursors)
+  
+(defhydra multiple-cursors-hydra (:hint nil)
+  "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_s_] Sort regions
+^ ^             ^ ^             [_q_] Quit
+"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/sort-regions)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil))
+
+
+(global-set-key (kbd "C-=") 'multiple-cursors-hydra/body)
+
 (provide 'iasoon-editor)
