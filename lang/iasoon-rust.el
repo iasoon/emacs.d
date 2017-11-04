54f9da1ffbaebd85e2ac5e420e1553f1e2716ea4
Ilion Beyst <ilion.beyst@gmail.com>
2017-11-04 23:47:40 +0100

    flycheck for rust

--- lang/iasoon-rust.el
+++ lang/iasoon-rust.el
@@ -8,5 +8,8 @@
   (setq compilation-ask-about-save nil)
   :diminish cargo-minor-mode)
 
+(use-package flycheck-rust
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
 
 (provide 'iasoon-rust)
