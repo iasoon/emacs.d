ee6a18d29fec26e255d98319ef0d760a9130c4ad
Ilion Beyst <ilion.beyst@gmail.com>
2017-11-06 18:44:05 +0100

    more useful dired listings

--- core/iasoon-dired.el
+++ core/iasoon-dired.el
@@ -2,6 +2,8 @@
 
 (define-key dired-mode-map "f" 'counsel-file-jump)
 
+(setq dired-listing-switches "-laGh1v --group-directories-first")
+
 (use-package dired-ranger
   :bind (:map dired-mode-map
               ("c" . dired-ranger-copy)
