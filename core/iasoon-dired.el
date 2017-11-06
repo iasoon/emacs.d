fe42d458d618c6fa22ccc45b47c7af2905fa48ff
Ilion Beyst <ilion.beyst@gmail.com>
2017-11-06 18:45:03 +0100

    don't prompt recursive actions

--- core/iasoon-dired.el
+++ core/iasoon-dired.el
@@ -4,6 +4,11 @@
 
 (setq dired-listing-switches "-laGh1v --group-directories-first")
 
+;; don't prompt for recursive actions
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+
+
 (use-package dired-ranger
   :bind (:map dired-mode-map
               ("c" . dired-ranger-copy)
