2595c6744501db617eaa2d32a8ce9b1fcd87d732
Ilion Beyst <ilion.beyst@gmail.com>
2017-12-22 13:52:50 +0100

    customize dired faces

--- core/iasoon-appearance.el
+++ core/iasoon-appearance.el
@@ -14,11 +14,6 @@
 ;; This is super annoying
 (blink-cursor-mode -1)
 
-(use-package nord-theme
-  :config
-  (setq nord-comment-brightness 15)
-  (setq nord-uniform-mode-lines t)
-  (load-theme 'nord t))
 
 ;;
 ;; Fringes
@@ -38,5 +33,38 @@
        "XX......"
        "XXX....."
        "XXXX....")))
+
+
+;;
+;; Theme
+;;
+
+(use-package nord-theme
+  :config
+  (setq nord-comment-brightness 15)
+  (setq nord-uniform-mode-lines t)
+  (load-theme 'nord t))
+
+
+(let ((class '((class color) (min-colors 89)))
+  (nord0 (if (display-graphic-p) "#2E3440" nil))
+  (nord1 (if (display-graphic-p) "#3B4252" "black"))
+  (nord2 (if (display-graphic-p) "#434C5E" "#434C5E"))
+  (nord3 (if (display-graphic-p) "#4C566A" "brightblack"))
+  (nord4 (if (display-graphic-p) "#D8DEE9" "#D8DEE9"))
+  (nord5 (if (display-graphic-p) "#E5E9F0" "white"))
+  (nord6 (if (display-graphic-p) "#ECEFF4" "brightwhite"))
+  (nord7 (if (display-graphic-p) "#8FBCBB" "cyan"))
+  (nord8 (if (display-graphic-p) "#88C0D0" "brightcyan"))
+  (nord9 (if (display-graphic-p) "#81A1C1" "blue"))
+  (nord10 (if (display-graphic-p) "#5E81AC" "brightblue"))
+  (nord11 (if (display-graphic-p) "#BF616A" "red"))
+  (nord12 (if (display-graphic-p) "#D08770" "brightyellow"))
+  (nord13 (if (display-graphic-p) "#EBCB8B" "yellow"))
+  (nord14 (if (display-graphic-p) "#A3BE8C" "green")))
+  (custom-theme-set-faces
+   'nord
+   `(dired-directory ((,class (:foreground ,nord9))))
+   `(dired-marked ((,class (:foreground ,nord13))))))
 
 (provide 'iasoon-appearance)
