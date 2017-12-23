7d4f6bbaed43a153756740a322f4c5835d8b0faa
Ilion Beyst <ilion.beyst@gmail.com>
2017-12-23 12:25:46 +0100

    use some of the new ivy features

--- core/iasoon-ivy.el
+++ core/iasoon-ivy.el
@@ -1,27 +1,42 @@
 (use-package ivy
   :config
-  ;; fuzzy matching
+
+  ;; use fuzzy matching
   (setq ivy-re-builders-alist
 	'((swiper . ivy--regex-plus)
 	  (t . ivy--regex-fuzzy)))
+
   ;; no initial inputs; fuzzy all the way
   (setq ivy-initial-inputs-alist nil)
+
   ;; dont list current and parent directory
   (setq ivy-extra-directories nil)
+
+  ;; allow selecting the prompt string
+  (setq ivy-use-selectable-prompt t)
+
+  ;; delayed updates for more repsonsivity
+  (setq counsel-async-filter-update-time 10000)
+  (setq ivy-dynamic-exhibit-delay-ms 20)
+
   (ivy-mode)
   :diminish ivy-mode)
+
+
 
 
 (use-package swiper
   :bind ("C-s" . swiper))
 
-
 (use-package counsel
   :config
+
   ;; fuzzy matching
   (use-package flx)
+
   ;; smex in counsel-M-x
   (use-package smex)
+
   (setq counsel-rg-base-command
         "rg -i -M 120 --no-heading --line-number --color never %s .")
   :bind
