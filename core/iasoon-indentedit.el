9806d0a60658c140e0398610e272eb93dc429f51
Ilion Beyst <ilion.beyst@gmail.com>
2017-12-25 19:04:01 +0100

    indentedit--opening-sexp

--- core/iasoon-indentedit.el
+++ core/iasoon-indentedit.el
@@ -3,6 +3,33 @@
     (back-to-indentation)
     (eolp)))
 
+(defun indentedit-forward (&optional direction)
+  (let ((direction (or direction (direction)))
+        (pos-indent (current-indentation)))
+    (while (and (zerop (forward-line direction))
+             (or (indentedit-line-blank-p)
+               (> (current-indentation) pos-indent))))))
+
+(defun indentedit--opening-sexp ()
+  (save-excursion
+    (let ( (beg (line-beginning-position))
+           (end (line-end-position))
+           (sexp t))
+      (move-beginning-of-line nil)
+      (while (progn
+               (setq sexp (sp-get-thing))
+               (when sexp (goto-char (sp-get sexp :end)))
+               (and sexp (>= end (point)))))
+      (when (<= beg (sp-get sexp :beg) end) sexp))))
+
+(defun indentedit--forward-line ()
+  (interactive)
+  (if-let ((sexp (indentedit--opening-sexp)))
+    (goto-char (sp-get sexp :end))
+    (let ((ok (zerop (forward-line 1))))
+      (when ok (back-to-indentation))
+      ok)))
+
 (defun indentedit-next ()
   (interactive)
   (let ((pos-indent (current-indentation)))
