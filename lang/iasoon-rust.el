407729f672c2da35c074f6c3f85040f54a819a08
Ilion Beyst <ilion.beyst@gmail.com>
2017-12-27 13:43:16 +0100

    return to racer-mode instead of ycmd

--- lang/iasoon-rust.el
+++ lang/iasoon-rust.el
@@ -1,12 +1,14 @@
-(use-package rust-mode
-  :config
-  (add-hook 'rust-mode-hook 'ycmd-mode))
+(use-package rust-mode)
 
 (use-package cargo
   :config
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
   (setq compilation-ask-about-save nil)
   :diminish cargo-minor-mode)
+
+(use-package racer
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode))
 
 (use-package flycheck-rust
   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
