4fb2e7078d42ff75fa311b5682671901897fd59b
Ilion Beyst <ilion.beyst@gmail.com>
2018-01-15 18:02:00 +0100

    use company for all completions at point

--- core/iasoon-completion.el
+++ core/iasoon-completion.el
@@ -14,10 +14,26 @@
   (add-to-list 'company-frontends 'company-tng-frontend)
   (company-tng-configure-default)
   (define-key company-active-map (kbd "<tab>") 'company-select-next)
-  (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+
+  (setq tab-always-indent 'complete)
+
+  (defvar completion-at-point-functions-saved nil)
+
+  (defun company-indent-for-tab-command (&optional arg)
+    (interactive "P")
+    (let ((completion-at-point-functions-saved completion-at-point-functions)
+          (completion-at-point-functions '(company-complete-common-wrapper)))
+      (indent-for-tab-command arg)))
+
+  (defun company-complete-common-wrapper ()
+    (let ((completion-at-point-functions completion-at-point-functions-saved))
+      (company-complete-common)))
+
+  :bind (:map company-mode-map
+         ("<tab>" . company-indent-for-tab-command)))
 
 ;; ycmd
-
 (use-package ycmd
   :config
   (setq ycmd-server-command `("python" ,(file-truename "~/src/ycmd/ycmd/")))
